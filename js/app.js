59bf4f587d7ccc66e27e90a8e45e776ba9de7d6d
cychowai <71240864+cychowai@users.noreply.github.com>
2021-12-27 16:31:11 +0800

    ssss-basic

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function () {
+    var DOM = {};
+    // DOM.threshold = $(".threshold");
+    DOM.noOfShares = $(".noOfShares");
+    var noOfShares = DOM.noOfShares.value;
+    console.log("noOfShares : ", noOfShares);
+    // var noOfShares = document.getElementById("noOfShares");
+    // var threshold = document.getElementById("threshold")
+    // var secret = document.getElementById("secret");
+    // 
+    // console.log("threshold : ", threshold);
+    // console.log("secret : ", secret);
+    // var noOfShares = document.getElementById("noOfShares").value;
+    // var noOfShares = document.getElementById("noOfShares").value;
+})();
---
+++ js/function.js
@@ -0,0 +1,7 @@
+splitSecret = function () {
+    document.getElementById("shares").value = "...Shares //todo";
+}
+
+combineShares = function () {
+    document.getElementById("showSecret").value = "...Secret //todo";
+}
