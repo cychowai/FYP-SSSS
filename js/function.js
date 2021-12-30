3831bce85f2e6ac2a0cd718b6954a02af5f606aa
cychowai <71240864+cychowai@users.noreply.github.com>
2021-12-30 19:18:08 +0800

    comment for SSSS

--- js/function.js
+++
@@ -1,7 +0,0 @@
-splitSecret = function () {
-    document.getElementById("shares").value = "...Shares //todo";
-}
-
-combineShares = function () {
-    document.getElementById("showSecret").value = "...Secret //todo";
-}
