00085fe2458c33bcc5c606d52130125797c85bd5
cychowai <71240864+cychowai@users.noreply.github.com>
2022-01-04 20:25:29 +0800

    checking for secret and shares

--- js/combineShares.js
+++ js/combineShares.js
@@ -4,11 +4,20 @@ combineShares = function () {
     console.log("threshold : ", threshold);
     var shares = document.getElementsByClassName("shares")[0].value;
     console.log("shares : ", shares);
+
+    //checking whether the shares exist
+    if (!shares) {
+        alert("Please enter the shares!");
+        return;
+    }
+
+    //secret that returns
+    var secret = 'secret';
     
     //todo
     //combine shares into secret with no of shares >= threshold
     
     
 
-    document.getElementById("showSecret").value = "secret here //todo";
+    document.getElementById("showSecret").value = secret;
 }
