f1684654474bf411ccde1b5764ebf057c842495b
cychowai <71240864+cychowai@users.noreply.github.com>
2022-04-27 15:25:19 +0800

    remove logging

--- js/image/generateImageShares.js
+++ js/image/generateImageShares.js
@@ -4,22 +4,12 @@ function generateImageShares(imgData, noOfShares, threshold, padLength) {
 		secret = intToHex(imgData.data[i]) + secret;
 	}
 
-	console.log(imgData.data[0]);
-	console.log(intToHex(imgData.data[0]));
-	console.log(imgData.data[imgData.data.length - 1]);
-	console.log(intToHex(imgData.data[imgData.data.length - 1]));
-	console.log(secret);
-
 	var shares = generateShares(secret, noOfShares, threshold, padLength);
-
 	var img_data_hex = new Array(noOfShares);
+
 	for (var i = 0; i < shares.length; i++) {
 		img_data_hex[i] = hexToInt(shares[i]);
 	}
-
-	console.log(secret.length);
-	console.log(shares);
-	console.log(img_data_hex);
 
 	return shares;
 };
--- js/image/imageSplitSecret.js
+++ js/image/imageSplitSecret.js
@@ -26,7 +26,6 @@ imageSplitSecret = function () {
 	console.log("defaultExps: ", defaultExps);
 	console.log("defaultLogs: ", defaultLogs);
 
-	//lcp:return if threshold > noOfShares
 	if (threshold > noOfShares) {
 		return;
 	}
@@ -35,24 +34,13 @@ imageSplitSecret = function () {
 
 	console.log(imgData);
 
-	console.log(imageLoader.src);
-
 	//split the secret into shares with no = noOfShares
 	var shares = generateImageShares(imgData, noOfShares, threshold, minPadLength);
 
-	/*
-	var link = document.createElement('a');
-	link.href = imageLoader.src;
-	link.download = 'Download.jpg';
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
-	*/
-
-	console.log(shares.length);
 	for (var i = 0; i < shares.length; i++) {
 		console.log(shares[i]);
 	}
+
 	for (var i = 0; i < shares.length; i++) {
 		var name = "Share" + parseInt(i + 1) + "_" + shares.length + "-" + imgData.width + "&" + imgData.height;
 		download(name, shares[i]);
