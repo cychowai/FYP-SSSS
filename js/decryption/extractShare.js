7661866261adbbbe10dcce64604a4ec1eb7dbca4
cychowai <71240864+cychowai@users.noreply.github.com>
2022-04-27 20:58:07 +0800

    Update extractShare.js

--- js/decryption/extractShare.js
+++ js/decryption/extractShare.js
@@ -1,28 +1,20 @@
 function extractShare(share) {
-    var bits, max, regexStr, shareComponents;
     var obj = {};
-    var id, idLen;
+    var bits = parseInt(share.substr(0, 1), 36);
+    var idLen = (Math.pow(2, bits) - 1).toString(defaultBase).length;
 
-    bits = parseInt(share.substr(0, 1), 36); 
-    max = Math.pow(2, bits) - 1;
-    idLen = (Math.pow(2, bits) - 1).toString(defaultBase).length;
     //regex used here
-    regexStr = "^([a-kA-K3-9]{1})([a-fA-F0-9]{" + idLen + "})([a-fA-F0-9]+)$";
-    shareComponents = new RegExp(regexStr).exec(share);
-
-    //logging
-    console.log(shareComponents[0]);
-    console.log(shareComponents[1]);
-    console.log(shareComponents[2]);
-    console.log(shareComponents[3]);
+    var regexStr = "^([a-kA-K3-9]{1})([a-fA-F0-9]{" + idLen + "})([a-fA-F0-9]+)$";
+    var shareComponents = new RegExp(regexStr).exec(share);
 
     if (shareComponents) {
-        id = parseInt(shareComponents[2], defaultBase);
+        var id = parseInt(shareComponents[2], defaultBase);
     }
+
     if (shareComponents && shareComponents[3]) {
         obj.bits = bits;
         obj.id = id;
         obj.data = shareComponents[3];
         return obj;
-    }   
+    }
 }
