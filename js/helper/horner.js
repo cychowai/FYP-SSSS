46636724e00ecb6cd0d105a0f495abad3172e9de
cychowai <71240864+cychowai@users.noreply.github.com>
2022-04-27 21:09:39 +0800

    Update horner.js

--- js/helper/horner.js
+++ js/helper/horner.js
@@ -1,13 +1,14 @@
 //Horner's method
 function horner(x, coeffs) {
-  var fx = 0;
+    var fx = 0;
 
-  for (var i = coeffs.length - 1; i >= 0; i--) {
-      if (fx !== 0) {
-          fx = defaultExps[(defaultLogs[x] + defaultLogs[fx]) % maxShares] ^ coeffs[i];
-      } else {
-          fx = coeffs[i];
-      }
-  }
-  return fx;
+    for (var i = coeffs.length - 1; i >= 0; i--) {
+        if (fx !== 0) {
+            fx = defaultExps[(defaultLogs[x] + defaultLogs[fx]) % maxShares] ^ coeffs[i];
+        } else {
+            fx = coeffs[i];
+        }
+    }
+
+    return fx;
 }
